865acec783674aff0827bcb52864f8a8371c7f1d
YiPort <yejinbin1024@163.com>
2023-04-02 19:40:26 +0800

    feat: 创建统一响应类和响应枚举

---
+++ yiport-framework/src/main/java/cn/yiport/enums/AppHttpCodeEnum.java
@@ -0,0 +1,29 @@
+package cn.yiport.enums;
+
+public enum AppHttpCodeEnum {
+    // 成功
+    SUCCESS(200,"操作成功"),
+    // 登录
+    NEED_LOGIN(401,"需要登录后操作"),
+    NO_OPERATOR_AUTH(403,"无权限操作"),
+    SYSTEM_ERROR(500,"出现错误"),
+    USERNAME_EXIST(501,"用户名已存在"),
+     PHONENUMBER_EXIST(502,"手机号已存在"), EMAIL_EXIST(503, "邮箱已存在"),
+    REQUIRE_USERNAME(504, "必需填写用户名"),
+    LOGIN_ERROR(505,"用户名或密码错误");
+    int code;
+    String msg;
+
+    AppHttpCodeEnum(int code, String errorMessage){
+        this.code = code;
+        this.msg = errorMessage;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
