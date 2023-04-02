663d4a0ee2aed6a843f7798189710f7eca9822a2
YiPort <yejinbin1024@163.com>
2023-04-02 20:00:42 +0800

    test: 跨域设置，临时允许任何域名跨域

---
+++ yiport-blog/src/main/java/cn/yiport/config/WebConfig.java
@@ -0,0 +1,32 @@
+package cn.yiport.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    
+    /** 
+    * @Description: 跨域设置
+    * @Author: YiPort
+    * @Date: 2023/4/2 
+    */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许cookie
+                .allowCredentials(true)
+                // 设置允许的请求方式
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                // 设置允许的header属性
+                .allowedHeaders("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+
+}
