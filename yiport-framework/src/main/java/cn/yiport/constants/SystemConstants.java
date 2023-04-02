f2d3d6319419cd93bb6bad396a62eba016290470
YiPort <yejinbin1024@163.com>
2023-04-02 20:07:53 +0800

    perf: 对热门文章优化处理，对Article类进行封装以及进行字面值处理

---
+++ yiport-framework/src/main/java/cn/yiport/constants/SystemConstants.java
@@ -0,0 +1,14 @@
+package cn.yiport.constants;
+
+public class SystemConstants
+{
+    /**
+     *  文章是草稿
+     */
+    public static final int ARTICLE_STATUS_DRAFT = 1;
+    /**
+     *  文章是正常分布状态
+     */
+    public static final int ARTICLE_STATUS_NORMAL = 0;
+    
+}
--- yiport-framework/src/main/java/cn/yiport/service/impl/ArticleServiceImpl.java
+++ yiport-framework/src/main/java/cn/yiport/service/impl/ArticleServiceImpl.java
@@ -1,14 +1,18 @@
 package cn.yiport.service.impl;
 
+import cn.yiport.constants.SystemConstants;
 import cn.yiport.domain.ResponseResult;
 import cn.yiport.domain.entity.Article;
+import cn.yiport.domain.vo.HotArticleVo;
 import cn.yiport.mapper.ArticleMapper;
 import cn.yiport.service.ArticleService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,7 +23,7 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper,Article> imple
         //查询热门文章 封装成ResponseResult返回
         LambdaQueryWrapper<Article> queryWrapper = new LambdaQueryWrapper<>();
         //必须是正式文章
-        queryWrapper.eq(Article::getStatus,0);
+        queryWrapper.eq(Article::getStatus, SystemConstants.ARTICLE_STATUS_NORMAL);
         //按照浏览量进行排序
         queryWrapper.orderByDesc(Article::getViewCount);
         //最多只查询10条
@@ -27,6 +31,15 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper,Article> imple
         page(page,queryWrapper);
 
         List<Article> articles = page.getRecords();
+
+        //bean拷贝
+        List<HotArticleVo> articleVos = new ArrayList<>();
+        for (Article article : articles) {
+            HotArticleVo vo = new HotArticleVo();
+            BeanUtils.copyProperties(article,vo);
+            articleVos.add(vo);
+        }
+
         return ResponseResult.okResult(articles);
     }
 }
