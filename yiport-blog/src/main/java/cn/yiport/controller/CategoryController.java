cfb14b8cbc91f0ce20f6e95b8af6eba56ce09767
YiPort <yejinbin1024@163.com>
2023-04-04 23:14:14 +0800

    feat: 新增查询分类列表功能

---
+++ yiport-blog/src/main/java/cn/yiport/controller/CategoryController.java
@@ -0,0 +1,21 @@
+package cn.yiport.controller;
+
+import cn.yiport.domain.ResponseResult;
+import cn.yiport.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/getCategoryList")
+    public ResponseResult getCategoryList(){
+       return categoryService.getCategoryList();
+    }
+}
---
+++ yiport-framework/src/main/java/cn/yiport/domain/entity/Category.java
@@ -0,0 +1,42 @@
+package cn.yiport.domain.entity;
+
+import java.util.Date;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 分类表(Category)表实体类
+ *
+ * @author YiPort
+ * @since 2023-04-03 10:10:55
+ */
+@SuppressWarnings("serial")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category  {
+    
+    private Long id;
+    //分类名
+    private String name;
+    //父分类id，如果没有父分类为-1
+    private Long pid;
+    //描述
+    private String description;
+    //状态0:正常,1禁用
+    private String status;
+    
+    private Long createBy;
+    
+    private Date createTime;
+    
+    private Long updateBy;
+    
+    private Date updateTime;
+    //删除标志（0代表未删除，1代表已删除）
+    private Integer delFlag;
+
+}
+
