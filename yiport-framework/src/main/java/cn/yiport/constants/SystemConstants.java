1e10d66890a61aafb0a678f8744b7de6b58320a5
YiPort <yejinbin1024@163.com>
2023-04-04 23:35:38 +0800

    perf: 实现查询分类列表功能

--- yiport-framework/src/main/java/cn/yiport/constants/SystemConstants.java
+++ yiport-framework/src/main/java/cn/yiport/constants/SystemConstants.java
@@ -10,5 +10,9 @@ public class SystemConstants
      *  文章是正常分布状态
      */
     public static final int ARTICLE_STATUS_NORMAL = 0;
-    
+
+    /**
+     * 分类是正常状态
+     */
+    public static final String STATUS_NORMAL = "0";
 }
---
+++ yiport-framework/src/main/java/cn/yiport/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,53 @@
+package cn.yiport.service.impl;
+
+import cn.yiport.constants.SystemConstants;
+import cn.yiport.domain.ResponseResult;
+import cn.yiport.domain.entity.Article;
+import cn.yiport.domain.entity.Category;
+import cn.yiport.domain.vo.CategoryVo;
+import cn.yiport.mapper.CategoryMapper;
+import cn.yiport.service.ArticleService;
+import cn.yiport.service.CategoryService;
+import cn.yiport.utils.BeanCopyUtils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 分类表(Category)表服务实现类
+ *
+ * @author YiPort
+ * @since 2023-04-03 10:10:55
+ */
+@Service("categoryService")
+public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+    @Autowired
+    private ArticleService articleService;
+
+    @Override
+    public ResponseResult getCategoryList() {
+        //查询文章表  状态为已发布的文章
+        LambdaQueryWrapper<Article> articleWrapper = new LambdaQueryWrapper<>();
+        articleWrapper.eq(Article::getStatus, SystemConstants.ARTICLE_STATUS_NORMAL);
+        List<Article> articleList = articleService.list(articleWrapper);
+        //获取文章的分类id，并且去重
+        Set<Long> categoryIds = articleList.stream()
+                .map(article -> article.getCategoryId())
+                .collect(Collectors.toSet());
+
+        //查询分类表
+        List<Category> categories = listByIds(categoryIds);
+        categories = categories.stream()
+                .filter(category -> SystemConstants.STATUS_NORMAL.equals(category.getStatus()))
+                .collect(Collectors.toList());
+        //封装vo
+        List<CategoryVo> categoryVos = BeanCopyUtils.copyBeanList(categories, CategoryVo.class);
+
+        return ResponseResult.okResult(categoryVos);
+    }
+}
