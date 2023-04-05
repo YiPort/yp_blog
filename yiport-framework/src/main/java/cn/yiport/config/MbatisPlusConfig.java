402a0906f7adb33afac5b5b989bc36da50836742
YiPort <yejinbin1024@163.com>
2023-04-05 22:08:56 +0800

    fix: 修复分页查询文章列表时没有分页效果的问题

---
+++ yiport-framework/src/main/java/cn/yiport/config/MbatisPlusConfig.java
@@ -0,0 +1,23 @@
+package cn.yiport.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MbatisPlusConfig {
+
+    
+    /** 
+    * @Description: MP支持分页配置
+    * @Author: YiPort
+    * @Date: 2023/4/5 
+    */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
