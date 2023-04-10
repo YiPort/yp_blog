00995cf4345192400d5600ec87634eae6001e879
YiPort <yejinbin1024@163.com>
2023-04-11 00:18:12 +0800

    feat: 新增登录校验过滤器，使用Token查询Redis判断是否已经登录过

--- yiport-framework/src/main/java/cn/yiport/config/SecurityConfig.java
+++ yiport-framework/src/main/java/cn/yiport/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package cn.yiport.config;
 
+import cn.yiport.filter.JwtAuthenticationTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,14 +10,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -32,6 +40,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         http.logout().disable();
+        //在登录前添加登录校验过滤器,把jwtAuthenticationTokenFilter添加到SpringSecurity的过滤器链中
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+
+
         //允许跨域
         http.cors();
     }
