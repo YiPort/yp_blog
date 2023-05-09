ad009ade92fe8eac8823733c8112eb235ce48875
YiPort <yejinbin1024@163.com>
2023-05-10 03:43:27 +0800

    refactor: 对评论者的名字进行查询，解决直接CopyBean导致没有评论者昵称的问题

--- yiport-framework/src/main/java/cn/yiport/service/impl/CommentServiceImpl.java
+++ yiport-framework/src/main/java/cn/yiport/service/impl/CommentServiceImpl.java
@@ -6,10 +6,12 @@ import cn.yiport.domain.vo.CommentVo;
 import cn.yiport.domain.vo.PageVo;
 import cn.yiport.mapper.CommentMapper;
 import cn.yiport.service.CommentService;
+import cn.yiport.service.UserService;
 import cn.yiport.utils.BeanCopyUtils;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,6 +25,8 @@ import java.util.List;
 @Service("commentService")
 public class CommentServiceImpl extends ServiceImpl<CommentMapper, Comment> implements CommentService {
 
+    @Autowired
+    private UserService userService;
 
     @Override
     public ResponseResult commentList(Long articleId, Integer pageNum, Integer pageSize) {
@@ -37,11 +41,28 @@ public class CommentServiceImpl extends ServiceImpl<CommentMapper, Comment> impl
         Page<Comment> page = new Page(pageNum,pageSize);
         page(page,queryWrapper);
 
-       List<CommentVo> commentVoList = BeanCopyUtils.copyBeanList(page.getRecords(),CommentVo.class);
+        List<CommentVo> commentVoList = toCommentVoList(page.getRecords());
 
         return ResponseResult.okResult(new PageVo(commentVoList,page.getTotal()));
     }
 
+
+    private List<CommentVo> toCommentVoList(List<Comment> list){
+        List<CommentVo> commentVos = BeanCopyUtils.copyBeanList(list, CommentVo.class);
+        //遍历vo集合
+        for (CommentVo commentVo : commentVos) {
+            //通过creatyBy查询用户的昵称并赋值
+            String nickName = userService.getById(commentVo.getCreateBy()).getNickName();
+            commentVo.setUsername(nickName);
+            //通过toCommentUserId查询用户的昵称并赋值
+            //如果toCommentUserId不为-1才进行查询
+            if(commentVo.getToCommentUserId()!=-1){
+                String toCommentUserName = userService.getById(commentVo.getToCommentUserId()).getNickName();
+                commentVo.setToCommentUserName(toCommentUserName);
+            }
+        }
+        return commentVos;
+    }
 }
 
 
