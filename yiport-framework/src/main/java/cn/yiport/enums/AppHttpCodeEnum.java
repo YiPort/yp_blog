dd3053480e7bd938c6fae042cac834aac9dd0ce5
YiPort <yejinbin1024@163.com>
2023-05-11 21:41:48 +0800

    feat: 新增AppHttpCodeEnum枚举类的字面值，用于添加评论时的数据校验

--- yiport-framework/src/main/java/cn/yiport/enums/AppHttpCodeEnum.java
+++ yiport-framework/src/main/java/cn/yiport/enums/AppHttpCodeEnum.java
@@ -8,9 +8,12 @@ public enum AppHttpCodeEnum {
     NO_OPERATOR_AUTH(403,"无权限操作"),
     SYSTEM_ERROR(500,"出现错误"),
     USERNAME_EXIST(501,"用户名已存在"),
-     PHONENUMBER_EXIST(502,"手机号已存在"), EMAIL_EXIST(503, "邮箱已存在"),
+    PHONENUMBER_EXIST(502,"手机号已存在"),
+    EMAIL_EXIST(503, "邮箱已存在"),
     REQUIRE_USERNAME(504, "必需填写用户名"),
-    LOGIN_ERROR(505,"用户名或密码错误");
+    LOGIN_ERROR(505,"用户名或密码错误"),
+    CONTENT_NOT_NULL(506,"评论内容不能为空");
+
     int code;
     String msg;
 
