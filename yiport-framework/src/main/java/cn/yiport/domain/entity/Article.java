e29fb3a47b312deca096722c356c20d6bb7a25ee
YiPort <yejinbin1024@163.com>
2023-05-16 12:27:02 +0800

    feat: 使用定时任务每隔10分钟把Redis中的浏览量更新到数据库中

--- yiport-framework/src/main/java/cn/yiport/domain/entity/Article.java
+++ yiport-framework/src/main/java/cn/yiport/domain/entity/Article.java
@@ -57,5 +57,9 @@ public class Article  {
     //删除标志（0代表未删除，1代表已删除）
     private Integer delFlag;
 
+    public Article(Long id, long viewCount) {
+        this.id=id;
+        this.viewCount=viewCount;
+    }
 }
 
