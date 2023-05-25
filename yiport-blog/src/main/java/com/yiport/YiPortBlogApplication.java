9feac81be4308b4777c0c435f38f04c8de153c8a
YiPort <yejinbin1024@163.com>
2023-05-25 10:59:29 +0800

    feat: Blog模块开启OpenFeign功能

--- yiport-blog/src/main/java/com/yiport/YiPortBlogApplication.java
+++ yiport-blog/src/main/java/com/yiport/YiPortBlogApplication.java
@@ -3,6 +3,7 @@ package com.yiport;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @MapperScan("com.yiport.mapper")
 @EnableScheduling
+@EnableFeignClients
 public class YiPortBlogApplication {
 
     public static void main(String[] args) {
