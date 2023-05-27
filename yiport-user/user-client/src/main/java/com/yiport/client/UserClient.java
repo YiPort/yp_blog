5fbed1cdf4ee0c2ec645bfd2372ef7ae21baee2a
YiPort <yejinbin1024@163.com>
2023-05-27 11:43:24 +0800

    fix: 出于安全考虑，关闭用户客户端的feign接口

--- yiport-user/user-client/src/main/java/com/yiport/client/UserClient.java
+++ yiport-user/user-client/src/main/java/com/yiport/client/UserClient.java
@@ -10,31 +10,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient("userservice")
 public interface UserClient {
 
-    /**
-     * 根据id返回user
-     * @param id
-     * @return
-     */
-    @GetMapping("/user/getUserById")
-    User getById(Long id);
+//    /**
+//     * 根据id返回user
+//     * @param id
+//     * @return
+//     */
+//    @GetMapping("/user/getUserById")
+//    User getById(Long id);
 
-    /**
-     * 个人信息查询
-     * 需要token请求头
-     * @return
-     */
-    @GetMapping("/user/userInfo")
-    ResponseResult userInfo();
+//    /**
+//     * 个人信息查询
+//     * 需要token请求头
+//     * @return
+//     */
+//    @GetMapping("/user/userInfo")
+//    ResponseResult userInfo();
 
-    /**
-     * 更新个人信息
-     * 需要token请求头
-     * @param user
-     * @return
-     */
-    @PutMapping("/user/userInfo")
-    ResponseResult updateUserInfo(@RequestBody User user);
-
+//    /**
+//     * 更新个人信息
+//     * 需要token请求头
+//     * @param user
+//     * @return
+//     */
+//    @PutMapping("/user/userInfo")
+//    ResponseResult updateUserInfo(@RequestBody User user);
 
 
 }
