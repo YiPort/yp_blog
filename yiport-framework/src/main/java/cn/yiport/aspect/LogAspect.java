7ade59fe0f5d564ab31dc821d9e6a94fa01a48ea
YiPort <yejinbin1024@163.com>
2023-05-29 01:24:50 +0800

    feat: 新增AOP实现日志记录功能

---
+++ yiport-framework/src/main/java/cn/yiport/aspect/LogAspect.java
@@ -0,0 +1,87 @@
+package cn.yiport.aspect;
+
+
+import cn.yiport.annotation.SystemLog;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+
+    @Pointcut("@annotation(cn.yiport.annotation.SystemLog)")
+    public void pt() {
+    }
+
+    @Around("pt()")
+    public Object priaLog(ProceedingJoinPoint joinPoint) {
+
+        Object ret = null;
+
+        try {
+            handBefore(joinPoint);
+            ret = joinPoint.proceed();
+            handAfter(ret);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            log.info("=======End=======" + System.lineSeparator());
+        }
+
+        return ret;
+    }
+
+    private void handAfter(Object ret) {
+        // 打印出参
+        log.info("Response       : {}", JSON.toJSONString(ret));
+
+
+    }
+
+    private void handBefore(ProceedingJoinPoint joinPoint) {
+
+        ServletRequestAttributes requestAttributes= (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+
+        SystemLog systemLog=getSystemLog(joinPoint);
+
+
+
+
+        log.info("=======Start=======");
+        // 打印请求 URL
+        log.info("URL            : {}",request.getRequestURI());
+        // 打印描述信息
+        log.info("BusinessName   : {}", systemLog.BusinessName());
+        // 打印 Http method
+        log.info("HTTP Method    : {}",request.getMethod() );
+        // 打印调用 controller 的全路径以及执行方法
+        log.info("Class Method   : {}.{}", joinPoint.getSignature().getDeclaringTypeName(),((MethodSignature)joinPoint.getSignature()).getName());
+        // 打印请求的 IP
+        log.info("IP             : {}",request.getRemoteHost());
+        // 打印请求入参
+        log.info("Request Args   : {}", JSON.toJSONString(joinPoint.getArgs()));
+
+
+    }
+
+    private SystemLog getSystemLog(ProceedingJoinPoint joinPoint) {
+        MethodSignature methodSignature= (MethodSignature) joinPoint.getSignature();
+        SystemLog systemLog = methodSignature.getMethod().getAnnotation(SystemLog.class);
+        return systemLog;
+
+    }
+
+
+}
