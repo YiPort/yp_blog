fdcd7be8a672ee6a708097fab84702e70022dc5f
YiPort <yejinbin1024@163.com>
2023-06-10 15:45:42 +0800

    perf: 重构查询分类逻辑，解决循环依赖问题

--- yiport-blog/src/main/java/com/yiport/domain/vo/CategoryVo.java
+++ yiport-blog/src/main/java/com/yiport/domain/vo/CategoryVo.java
@@ -11,7 +11,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryVo {
+    /**
+     * 主键
+     */
     private Long id;
-    //分类名
+
+    /**
+     * 分类名
+     */
     private String name;
+
+    /**
+     * 父分类id，如果没有父分类为-1
+     */
+    private Long pid;
+
+    /**
+     * 描述
+     */
+    private String description;
 }
--- yiport-blog/src/main/java/com/yiport/service/impl/CategoryServiceImpl.java
+++ yiport-blog/src/main/java/com/yiport/service/impl/CategoryServiceImpl.java
@@ -1,22 +1,22 @@
 package com.yiport.service.impl;
 
-import com.yiport.constants.SystemConstants;
 import com.yiport.domain.ResponseResult;
-import com.yiport.domain.entity.Article;
 import com.yiport.domain.entity.Category;
 import com.yiport.domain.vo.CategoryVo;
 import com.yiport.mapper.CategoryMapper;
-import com.yiport.service.ArticleService;
 import com.yiport.service.CategoryService;
 import com.yiport.utils.BeanCopyUtils;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yiport.utils.RedisCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+
+
+import static com.yiport.constants.SystemConstants.STATUS_NORMAL;
 
 /**
  * 分类表(Category)表服务实现类
@@ -26,28 +26,21 @@ import java.util.stream.Collectors;
  */
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+    @Resource
+    private CategoryMapper categoryMapper;
+
     @Autowired
-    private ArticleService articleService;
+    private RedisCache redisCache;
 
     @Override
     public ResponseResult getCategoryList() {
-        //查询文章表  状态为已发布的文章
-        LambdaQueryWrapper<Article> articleWrapper = new LambdaQueryWrapper<>();
-        articleWrapper.eq(Article::getStatus, SystemConstants.ARTICLE_STATUS_NORMAL);
-        List<Article> articleList = articleService.list(articleWrapper);
-        //获取文章的分类id，并且去重
-        Set<Long> categoryIds = articleList.stream()
-                .map(article -> article.getCategoryId())
-                .collect(Collectors.toSet());
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
 
-        //查询分类表
-        List<Category> categories = listByIds(categoryIds);
-        categories = categories.stream()
-                .filter(category -> SystemConstants.STATUS_NORMAL.equals(category.getStatus()))
-                .collect(Collectors.toList());
-        //封装vo
-        List<CategoryVo> categoryVos = BeanCopyUtils.copyBeanList(categories, CategoryVo.class);
+        // 是正常状态的分类
+        queryWrapper.eq(Category::getStatus, STATUS_NORMAL);
+        List<Category> categories = categoryMapper.selectList(queryWrapper);
 
-        return ResponseResult.okResult(categoryVos);
+        List<CategoryVo> categoryListVOS = BeanCopyUtils.copyBeanList(categories, CategoryVo.class);
+        return ResponseResult.okResult(categoryListVOS);
     }
 }
