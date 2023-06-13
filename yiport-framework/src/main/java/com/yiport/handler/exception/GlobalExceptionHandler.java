cf1e97a1d5919a751517726ca9d1d7a1143b105d
YiPort <yejinbin1024@163.com>
2023-06-13 20:47:23 +0800

    chore: 修改异常提示信息

--- yiport-framework/src/main/java/com/yiport/handler/exception/GlobalExceptionHandler.java
+++ yiport-framework/src/main/java/com/yiport/handler/exception/GlobalExceptionHandler.java
@@ -30,6 +30,6 @@ public class GlobalExceptionHandler {
         //打印异常信息
         log.error("出现了异常:",e);
         //从异常对象中获取提示信息封装返回
-        return ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR.getCode(),e.getMessage());
+        return ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR.getCode(), "系统异常");
     }
 }
