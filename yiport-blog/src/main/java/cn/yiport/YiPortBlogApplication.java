ccad88825bde479e2e4d5c6f2d7f672f4ed84173
YiPort <yejinbin1024@163.com>
2023-06-17 12:06:19 +0800

    feat: 在应用启动时把博客的浏览量存储到redis中

--- yiport-blog/src/main/java/cn/yiport/YiPortBlogApplication.java
+++ yiport-blog/src/main/java/cn/yiport/YiPortBlogApplication.java
@@ -3,12 +3,14 @@ package cn.yiport;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @Author YiPort
  */
 @SpringBootApplication
 @MapperScan("cn.yiport.mapper")
+@EnableScheduling
 public class YiPortBlogApplication {
 
     public static void main(String[] args) {
