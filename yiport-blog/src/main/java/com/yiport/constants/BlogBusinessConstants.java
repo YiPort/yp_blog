55f6073b2e24111024a573b1a7b2d0d5dfd7ee2c
YiPort <yejinbin1024@163.com>
2023-06-19 20:35:19 +0800

    feat: 新增用户业务字面值以及调整博客主体业务字面值

--- yiport-blog/src/main/java/com/yiport/constants/BlogBusinessConstants.java
+++ yiport-blog/src/main/java/com/yiport/constants/BlogBusinessConstants.java
@@ -5,11 +5,11 @@ public class BlogBusinessConstants {
     /**
      * 文章访问量
      */
-    public static final String ARTICLE_VIEWCOUNT = "article:viewCount";
+    public static final String ARTICLE_VIEWCOUNT = "ypblog:article:viewCount";
 
     /**
      * 文章编辑记录
      */
-    public static final String ARTICLE_EDITLIST = "article:editList:";
+    public static final String ARTICLE_EDITLIST = "ypblog:article:editList:";
 
 }
---
+++ yiport-user/user-service/src/main/java/com/yiport/constent/UserBusinessConstants.java
@@ -0,0 +1,10 @@
+package com.yiport.constent;
+
+public class UserBusinessConstants {
+
+    /**
+     * 博客验证码
+     */
+    public static final String CAPTCHA_CODES = "ypblog:user:captcha_codes:";
+
+}
