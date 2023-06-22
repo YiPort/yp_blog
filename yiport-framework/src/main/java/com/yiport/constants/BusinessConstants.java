cb37e66d373905dbcdde7b09dddae1b1cff32cc9
YiPort <yejinbin1024@163.com>
2023-06-22 20:25:37 +0800

    chore: 修改博客登录相关的字面值

--- yiport-framework/src/main/java/com/yiport/constants/BusinessConstants.java
+++ yiport-framework/src/main/java/com/yiport/constants/BusinessConstants.java
@@ -4,13 +4,13 @@ public class BusinessConstants {
     /**
      * 博客登录信息
      */
-    public static final String BLOG_LOGIN = "ypblog:login:";
+    public static final String BLOG_LOGIN = "ypblog:user:login:";
     /**
      * 博客Token信息
      */
-    public static final String BLOG_TOKEN = "ypblog:token:";
+    public static final String BLOG_TOKEN = "ypblog:user:token:";
     /**
      * 管理员权限信息
      */
-    public static final String BLOG_ADMIN = "ypblog:admin:";
+    public static final String BLOG_ADMIN = "ypblog:user:admin:";
 }
