0e33b949145dd8ea06a65c6c168de96e1cf2616d
YiPort <yejinbin1024@163.com>
2023-06-25 22:59:14 +0800

    feat: 新增收藏表SQL

---
+++ sql/yp_collection.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `yp_collection`;
+
+CREATE TABLE `yp_collection`
+(
+    `id`          BIGINT(200) NOT NULL AUTO_INCREMENT,
+    `article_id`  BIGINT(20) DEFAULT NULL COMMENT '文章id',
+    `create_by`   BIGINT(20) DEFAULT NULL COMMENT '创建人的用户id',
+    `create_time` DATETIME DEFAULT CURRENT_TIMESTAMP NULL COMMENT '创建时间',
+    `update_by`   BIGINT(20) DEFAULT NULL COMMENT '更新人',
+    `update_time` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL COMMENT '最后编辑时间',
+    `del_flag`    INT(1) DEFAULT '0' COMMENT '删除标志（0代表未删除，1代表已删除）',
+    PRIMARY KEY (`id`)
+) ENGINE=INNODB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COMMENT='收藏表';
