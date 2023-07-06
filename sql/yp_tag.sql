5e7dfddf18ea9f4c4bc219ff62490f310377dcfb
YiPort <yejinbin1024@163.com>
2023-07-06 17:25:53 +0800

    feat: 创建标签的SQL表

---
+++ sql/yp_tag.sql
@@ -0,0 +1,43 @@
+/*
+SQLyog v10.2 
+MySQL - 5.7.36 : Database - yp_blog
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`yp_blog` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `yp_blog`;
+
+/*Table structure for table `yp_tag` */
+
+DROP TABLE IF EXISTS `yp_tag`;
+
+CREATE TABLE `yp_tag` (
+                          `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                          `name` varchar(128) DEFAULT NULL COMMENT '标签名',
+                          `create_by` bigint(20) DEFAULT NULL,
+                          `create_time` datetime DEFAULT NULL,
+                          `update_by` bigint(20) DEFAULT NULL,
+                          `update_time` datetime DEFAULT NULL,
+                          `del_flag` int(1) DEFAULT '0' COMMENT '删除标志（0代表未删除，1代表已删除）',
+                          `remark` varchar(500) DEFAULT NULL COMMENT '备注',
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COMMENT='标签';
+
+/*Data for the table `yp_tag` */
+
+insert  into `yp_tag`(`id`,`name`,`create_by`,`create_time`,`update_by`,`update_time`,`del_flag`,`remark`) values (1,'Mybatis',NULL,NULL,NULL,'2023-06-11 09:20:50',0,'weqwe'),(2,'asdas',NULL,'2023-06-11 09:20:55',NULL,'2023-06-11 09:20:55',1,'weqw'),(3,'weqw',NULL,'2023-06-11 09:21:07',NULL,'2023-06-11 09:21:07',1,'qweqwe'),(4,'Java',NULL,'2023-06-13 15:22:43',NULL,'2023-06-13 15:22:43',0,'sdad'),(5,'WAD',NULL,'2023-06-13 15:22:47',NULL,'2023-06-13 15:22:47',0,'ASDAD');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
