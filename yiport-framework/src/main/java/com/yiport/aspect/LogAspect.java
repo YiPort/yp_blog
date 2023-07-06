e793d393e40866579d0453d7c080ccf9ac31e3fb
YiPort <yejinbin1024@163.com>
2023-07-06 21:32:12 +0800

    fix: 修复因为日志捕获处理异常后，全局异常处理器失效的bug

--- yiport-framework/src/main/java/com/yiport/aspect/LogAspect.java
+++ yiport-framework/src/main/java/com/yiport/aspect/LogAspect.java
@@ -25,7 +25,7 @@ public class LogAspect {
     }
 
     @Around("pt()")
-    public Object priaLog(ProceedingJoinPoint joinPoint) {
+    public Object priaLog(ProceedingJoinPoint joinPoint) throws Throwable {
 
         Object ret = null;
 
@@ -33,8 +33,6 @@ public class LogAspect {
             handBefore(joinPoint);
             ret = joinPoint.proceed();
             handAfter(ret);
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
         } finally {
             log.info("=======End=======" + System.lineSeparator());
         }
