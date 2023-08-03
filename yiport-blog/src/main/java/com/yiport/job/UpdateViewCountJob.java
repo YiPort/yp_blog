da6c33dd56576c0f0ffe0addaf04135395711cf4
YiPort <yejinbin1024@163.com>
2023-08-03 21:16:49 +0800

    refactor: 重构浏览量同步持久化的定时任务

--- yiport-blog/src/main/java/com/yiport/job/UpdateViewCountJob.java
+++ yiport-blog/src/main/java/com/yiport/job/UpdateViewCountJob.java
@@ -1,8 +1,10 @@
 package com.yiport.job;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.yiport.domain.entity.Article;
 import com.yiport.service.ArticleService;
 import com.yiport.utils.RedisCache;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,8 +14,10 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.yiport.constants.BlogBusinessConstants.ARTICLE_VIEWCOUNT;
+import static com.yiport.constants.SystemConstants.RELEASE;
 
 @Component
+@Slf4j
 public class UpdateViewCountJob {
 
     @Autowired
@@ -22,17 +26,29 @@ public class UpdateViewCountJob {
     @Autowired
     private ArticleService articleService;
 
+
+    /**
+     * 持久化浏览量定时任务，每隔十分钟执行一次
+     */
     @Scheduled(cron = "0 0/10 * * * ?")
     public void updateViewCount() {
+        log.info("=======开始持久化=======");
+        LambdaQueryWrapper<Article> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Article::getStatus, RELEASE);
+        List<Article> articles = articleService.list(queryWrapper);
+
         Map<String, Integer> viewCountMap = redisCache.getCacheMap(ARTICLE_VIEWCOUNT);
 
-        List<Article> articles = viewCountMap.entrySet()
-                .stream().map(entry -> (new
-                        Article(Long.valueOf(entry.getKey()),entry.getValue().longValue())))
-                .collect(Collectors.toList());
+        articles.forEach(article ->
+        {
+            String id = article.getId().toString();
+            Long viewCount = Long.valueOf(viewCountMap.get(id));
+            article.setViewCount(viewCount);
+        });
 
         articleService.updateBatchById(articles);
 
+        log.info("=======持久化完成=======");
 
     }
 
