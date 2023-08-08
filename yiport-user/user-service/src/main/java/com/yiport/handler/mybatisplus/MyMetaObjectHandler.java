d3e951cac8158dbbe5fdf66106007f3326fa4533
YiPort <yejinbin1024@163.com>
2023-08-08 22:53:59 +0800

    fix: 元数据使用LocalDateTime生成字符串插入时间相关字段

--- yiport-user/user-service/src/main/java/com/yiport/handler/mybatisplus/MyMetaObjectHandler.java
+++ yiport-user/user-service/src/main/java/com/yiport/handler/mybatisplus/MyMetaObjectHandler.java
@@ -5,7 +5,8 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
@@ -14,16 +15,21 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         Long userId = null;
 
         userId = SecurityUtils.getUserId();
+        // 获取时间戳,设置创建时间
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
-        this.setFieldValByName("createTime", new Date(), metaObject);
-        this.setFieldValByName("createBy",userId , metaObject);
-        this.setFieldValByName("updateTime", new Date(), metaObject);
+        this.setFieldValByName("createTime", createTime, metaObject);
+        this.setFieldValByName("createBy", userId, metaObject);
+        this.setFieldValByName("updateTime", createTime, metaObject);
         this.setFieldValByName("updateBy", userId, metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.setFieldValByName("updateTime", new Date(), metaObject);
+        // 获取时间戳,设置创建时间
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        this.setFieldValByName("updateTime", createTime, metaObject);
         this.setFieldValByName(" ", SecurityUtils.getUserId(), metaObject);
     }
 }
