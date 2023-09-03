ba7fde19afafebd3e7e78ba6e743466ed0808c55
YiPort <yejinbin1024@163.com>
2023-09-03 12:26:46 +0800

    refactor: 同步Comment表新增字段所对应的成员变量

--- yiport-blog/src/main/java/com/yiport/domain/entity/Comment.java
+++ yiport-blog/src/main/java/com/yiport/domain/entity/Comment.java
@@ -1,7 +1,5 @@
 package com.yiport.domain.entity;
 
-import java.util.Date;
-
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.AllArgsConstructor;
@@ -9,6 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+
 /**
  * 评论表(Comment)表实体类
  *
@@ -20,7 +21,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 @AllArgsConstructor
 @NoArgsConstructor
 @TableName("yp_comment")
-public class Comment {
+public class Comment implements Serializable{
     @TableId
     private Long id;
 
@@ -59,6 +60,17 @@ public class Comment {
      */
     private String createNick;
 
+
+    /**
+     * 创建人的头像
+     */
+    private String avatar;
+
+    /**
+     * 评论标签
+     */
+    private String label;
+
     /**
      * 评论状态 0-未精选，1-精选评论
      */
@@ -93,6 +105,87 @@ public class Comment {
      */
     private Integer delFlag;
 
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Comment other = (Comment) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+                && (this.getType() == null ? other.getType() == null : this.getType().equals(other.getType()))
+                && (this.getArticleId() == null ? other.getArticleId() == null : this.getArticleId().equals(other.getArticleId()))
+                && (this.getRootId() == null ? other.getRootId() == null : this.getRootId().equals(other.getRootId()))
+                && (this.getContent() == null ? other.getContent() == null : this.getContent().equals(other.getContent()))
+                && (this.getToCommentUserId() == null ? other.getToCommentUserId() == null : this.getToCommentUserId().equals(other.getToCommentUserId()))
+                && (this.getToCommentId() == null ? other.getToCommentId() == null : this.getToCommentId().equals(other.getToCommentId()))
+                && (this.getCreateBy() == null ? other.getCreateBy() == null : this.getCreateBy().equals(other.getCreateBy()))
+                && (this.getCreateNick() == null ? other.getCreateNick() == null : this.getCreateNick().equals(other.getCreateNick()))
+                && (this.getAvatar() == null ? other.getAvatar() == null : this.getAvatar().equals(other.getAvatar()))
+                && (this.getLabel() == null ? other.getLabel() == null : this.getLabel().equals(other.getLabel()))
+                && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
+                && (this.getUpdateBy() == null ? other.getUpdateBy() == null : this.getUpdateBy().equals(other.getUpdateBy()))
+                && (this.getUpdateTime() == null ? other.getUpdateTime() == null : this.getUpdateTime().equals(other.getUpdateTime()))
+                && (this.getDelFlag() == null ? other.getDelFlag() == null : this.getDelFlag().equals(other.getDelFlag()));
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getType() == null) ? 0 : getType().hashCode());
+        result = prime * result + ((getArticleId() == null) ? 0 : getArticleId().hashCode());
+        result = prime * result + ((getRootId() == null) ? 0 : getRootId().hashCode());
+        result = prime * result + ((getContent() == null) ? 0 : getContent().hashCode());
+        result = prime * result + ((getToCommentUserId() == null) ? 0 : getToCommentUserId().hashCode());
+        result = prime * result + ((getToCommentId() == null) ? 0 : getToCommentId().hashCode());
+        result = prime * result + ((getCreateBy() == null) ? 0 : getCreateBy().hashCode());
+        result = prime * result + ((getCreateNick() == null) ? 0 : getCreateNick().hashCode());
+        result = prime * result + ((getAvatar() == null) ? 0 : getAvatar().hashCode());
+        result = prime * result + ((getLabel() == null) ? 0 : getLabel().hashCode());
+        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
+        result = prime * result + ((getUpdateBy() == null) ? 0 : getUpdateBy().hashCode());
+        result = prime * result + ((getUpdateTime() == null) ? 0 : getUpdateTime().hashCode());
+        result = prime * result + ((getDelFlag() == null) ? 0 : getDelFlag().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", type=").append(type);
+        sb.append(", articleId=").append(articleId);
+        sb.append(", rootId=").append(rootId);
+        sb.append(", content=").append(content);
+        sb.append(", toCommentUserId=").append(toCommentUserId);
+        sb.append(", toCommentId=").append(toCommentId);
+        sb.append(", createBy=").append(createBy);
+        sb.append(", createNick=").append(createNick);
+        sb.append(", avatar=").append(avatar);
+        sb.append(", label=").append(label);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateBy=").append(updateBy);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append(", delFlag=").append(delFlag);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
 
 }
 
