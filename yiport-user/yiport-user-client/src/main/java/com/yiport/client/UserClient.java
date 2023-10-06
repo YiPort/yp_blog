659eb88af7191e32fe8b64aeacaca2e585cd60bf
YiPort <yejinbin1024@163.com>
2023-10-06 13:45:11 +0800

    fix: 用户client服务引入博客框架的通用模块

--- yiport-user/yiport-user-client/src/main/java/com/yiport/client/UserClient.java
+++ yiport-user/yiport-user-client/src/main/java/com/yiport/client/UserClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("userservice")
+@FeignClient("user-service")
 public interface UserClient {
 
 //    /**
