49f68f6b48f94e581d11569c7c93ecebb655368c
YiPort <yejinbin1024@163.com>
2023-10-16 01:49:41 +0800

    feat: 新增Markdown工具类

---
+++ yiport-framework/yiport-framework-utils/src/main/java/com/yiport/utils/MarkdownUtils.java
@@ -0,0 +1,51 @@
+package com.yiport.utils;
+
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Whitelist;
+
+public class MarkdownUtils {
+
+
+    public static String markdown2PlainText (String md){
+        String html = markdown2Html(md);
+        return html2PlainText(html);
+    }
+
+    public static String markdown2Html(String md) {
+        MutableDataSet options = new MutableDataSet();
+
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        Node document = parser.parse(md);
+        String html = renderer.render(document);
+        return html;
+    }
+
+
+    public static String html2PlainText (String html)
+    {
+        if (StringUtils.isEmpty(html))
+        {
+            return "";
+        }
+
+        Document document = Jsoup.parse(html);
+        Document.OutputSettings outputSettings = new Document.OutputSettings().prettyPrint(false);
+        document.outputSettings(outputSettings);
+        document.select("br").append("\\n");
+        document.select("p").prepend("\\n");
+        document.select("p").append("\\n");
+        String newHtml = document.html().replaceAll("\\\\n", "\n");
+        String plainText = Jsoup.clean(newHtml, "", Whitelist.none(), outputSettings);
+        String result = StringEscapeUtils.unescapeHtml(plainText.trim());
+        return result;
+    }
+}
