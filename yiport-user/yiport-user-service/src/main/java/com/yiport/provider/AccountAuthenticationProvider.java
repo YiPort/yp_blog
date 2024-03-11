d77ce22c23f2263bb25b2f056626fb883102cf25
YiPort <yejinbin1024@163.com>
2024-03-11 20:54:06 +0800

    fix: 修复token校验bug和登录校验bug

--- yiport-user/yiport-user-service/src/main/java/com/yiport/provider/AccountAuthenticationProvider.java
+++ yiport-user/yiport-user-service/src/main/java/com/yiport/provider/AccountAuthenticationProvider.java
@@ -1,14 +1,27 @@
 package com.yiport.provider;
 
 import com.yiport.domain.entity.LoginUser;
+import com.yiport.exception.SystemException;
+import com.yiport.service.LoginInfoService;
 import com.yiport.service.impl.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.yiport.constent.ExceptionDescription.ACCOUNT_DEACTIVATE;
+import static com.yiport.constent.ExceptionDescription.ACCOUNT_PASSWORD_ERROR;
+import static com.yiport.constent.ExceptionDescription.PASSWORD_ERROR;
+import static com.yiport.constent.UserConstant.FAIL;
+import static com.yiport.constent.UserConstant.LOGIN_BY_ACCOUNT;
 
 /**
  * 账号密码认证器
@@ -19,6 +32,12 @@ import org.springframework.stereotype.Component;
 public class AccountAuthenticationProvider implements AuthenticationProvider
 {
     private final UserDetailsServiceImpl userDetailsService;
+    private final LoginInfoService loginInfoService;
+    private final HttpServletRequest request;
+
+    @Lazy
+    @Autowired
+    private PasswordEncoder bCryptPasswordEncoder;
 
     /**
      * 进行账号密码认证
@@ -27,8 +46,21 @@ public class AccountAuthenticationProvider implements AuthenticationProvider
     public Authentication authenticate(Authentication authentication) throws AuthenticationException
     {
         String userAccount = authentication.getPrincipal().toString();
+        String userPassword = authentication.getCredentials().toString();
         // 获取用户信息
         LoginUser loginUser = (LoginUser) userDetailsService.loadUserByUsername(userAccount);
+        // 密码校验
+        if (!bCryptPasswordEncoder.matches(userPassword, loginUser.getUser().getPassword()))
+        {
+            loginInfoService.recordLoginInfo(userAccount, FAIL, PASSWORD_ERROR, LOGIN_BY_ACCOUNT, request);
+            throw new SystemException(ACCOUNT_PASSWORD_ERROR);
+        }
+        // 账号状态校验
+        if (!loginUser.isEnabled())
+        {
+            loginInfoService.recordLoginInfo(userAccount, FAIL, ACCOUNT_DEACTIVATE, LOGIN_BY_ACCOUNT, request);
+            throw new SystemException(ACCOUNT_DEACTIVATE);
+        }
         // 返回经过认证的Authentication
         UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(
                 loginUser, null, loginUser.getAuthorities());
